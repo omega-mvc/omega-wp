a17be70fd35d744aa644fe22f810373876387d39
omega-mvc <agisoftt@gmail.com>
2026-04-24 16:42:21 +0200

    Minor fix

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 use Omega\Application\Application;
 
-$app = new Application(dirname(__DIR__), 'omega-wp');
+$app = new Application(
+    config: ['base_path' => dirname(__DIR__)],
+    id: 'omega-wp'
+);
 
 $app->bootstrap();
 
