cef73632a558c6c78eb3134dfb9e20baf976861b
omega-mvc <agisoftt@gmail.com>
2026-05-04 16:48:45 +0200

    Minor fix

--- routes/admin.php
+++ routes/admin.php
@@ -6,10 +6,6 @@ use Omega\Routing\Facade\Route;
 defined('ABSPATH') || exit;
 
 //You can access this example route with: /wp-admin/admin.php?page=my-page-id&path=/path-example";
-/**Route::page('my-page-id')->guards(['edit_posts'])->group(function () {
-	Route::get('/path-example', [TaskController::class, 'index']);
-});*/
-
-Route::page('my-page-id')->group(function () {
-    Route::get('/path-example', [TaskController::class, 'create']);
+Route::page('my-page-id')->guards(['edit_posts'])->group(function () {
+	Route::get('/path-example', [TaskController::class, 'create']);
 });
--- routes/api.php
+++ routes/api.php
@@ -5,10 +5,6 @@ use Omega\Routing\Facade\Route;
 
 defined('ABSPATH') || exit;
 
-/**Route::prefix('omega-wp/v1')->guards(['edit_posts'])->group(function () {
+Route::prefix('omega-wp/v1')->guards(['edit_posts'])->group(function () {
 	Route::get('/tasks', [TaskController::class, 'index']);
-});*/
-
-Route::prefix('omega-wp/v1')->group(function () {
-    Route::get('tasks', [TaskController::class, 'index']);
 });
