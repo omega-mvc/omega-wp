5691b9d0dcee35ac4c3f0e1dcdb2922086eff51c
omega-mvc <agisoftt@gmail.com>
2026-08-04 14:26:41 +0200

    Add env support

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,9 @@
 declare(strict_types=1);
 
 use Omega\Application\ApplicationFactory;
+use Omega\Environment\Env;
+
+Env::load(dirname(__DIR__));
 
 $app = ApplicationFactory::createPlugin(
     id: 'omega-wp',
