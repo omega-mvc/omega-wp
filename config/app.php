9bccbd73ce1ae27b9cddb612bdf8f6c4f195c31b
omega-mvc <agisoftt@gmail.com>
2026-08-06 11:29:26 +0200

    Add complete env support

--- config/app.php
+++ config/app.php
@@ -2,7 +2,11 @@
 
 declare(strict_types=1);
 
+use function Omega\Environment\env;
+
 return [
+	'environment' => env('APP_ENV', ''),
+	'debug'       => env('APP_DEBUG', ''),
 	'admin' => [
 		'menu'  => App\Admin\Menu::class,
 		'setup' => App\Admin\Setup::class
